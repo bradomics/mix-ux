19c787e20b5decfaff90118b5a7d184c22d001d0
Brad Hall <brad@Brads-MBP.lan>
2021-01-12 06:29:11 -0600

    Finally controls are working

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import ButtonStories from './Button.stories';
 
 
-export default function Button({ Button: { active, state }, onArchiveButton, onPinButton }) {
+export default function Button({ loading }) {
   return (
     <div className="list-item">
       <label className="checkbox">
       </label>
+      {
+        loading && <h1>Loading = true</h1>
+      }
       <div className="title">
       </div>
     </div>
@@ -14,9 +18,9 @@ export default function Button({ Button: { active, state }, onArchiveButton, onP
 }
 
 Button.propTypes = {
-    Button: PropTypes.shape({
-        active: PropTypes.bool
-    }),
-    onArchiveButton: PropTypes.func,
-    onPinButton: PropTypes.func
+  loading: PropTypes.bool
+}
+
+Button.defaultProps = {
+  loading: false
 }
--- src/components/Button.stories.js
+++ src/components/Button.stories.js
@@ -9,29 +9,8 @@ export default {
 const Template = args => <Button {...args} />;
 
 export const Default = Template.bind({});
+
+
 Default.args = {
-  Button: {
-    active: false
-  },
-};
-
-// export const Active = Template.bind({});
-// Active.args = {
-//   active: true,
-// };
-
-export const Pinned = Template.bind({});
-Pinned.args = {
-  Button: {
-    ...Default.args.Button,
-    state: 'Button_PINNED',
-  },
-};
-
-export const Archived = Template.bind({});
-Archived.args = {
-  Button: {
-    ...Default.args.Button,
-    state: 'Button_ARCHIVED',
-  },
+    loading: false
 };
