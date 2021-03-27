2b6792e8f362aaf819dcef0499ea04a3284ca8f3
Brad Hall <brad@Brads-MBP.lan>
2021-03-27 18:43:38 -0500

    Added navbar component

---
+++ src/components/Navbar.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import NavbarStories from './Navbar.stories';
+import './Navbar/Navbar.css'
+
+
+export default function Navbar({ title, navbarItems, className }) {
+  return (
+    <div className={`${className}`}>
+        <h2 className="navbar-title">{title}</h2>
+        {
+            navbarItems.map((navbarItem => {
+                {console.log('navbar -> ', navbarItem)}
+                return (<a class="navbar-item">
+                    {navbarItem}
+                </a>)
+            }))
+        }
+    </div>
+  );
+}
+
+Navbar.propTypes = {
+  title: PropTypes.string,
+  navbarItems: PropTypes.array,
+  active: PropTypes.bool
+}
+
+Navbar.defaultProps = {
+  title: 'Navbar',
+  navbarItems: ['home'],
+  active: false
+}
---
+++ src/components/Navbar.stories.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import Navbar from './Navbar';
+
+export default {
+  component: Navbar,
+  title: 'Navbar'
+};
+
+const Template = args => <Navbar {...args} className="navbar" />;
+
+const DarkTemplate = args => <Navbar {...args} className="navbar navbar-dark"/>;
+
+
+export const Default = Template.bind({});
+
+export const Dark = DarkTemplate.bind({});
+
+
+Default.args = {
+    navbarItems: ['Home', 'Shop'],
+    active: false,
+    title: 'Navbar'
+};
+
+Dark.args = {
+  navbarItems: ['Home', 'Shop'],
+  active: false,
+  title: 'Dark Navbar'
+};
