ee121e82a02cfac8e131b97939c2102ac64c2baf
= <=>
2021-04-03 23:29:21 -0500

    updated github action yaml file

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -2,7 +2,6 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './Button.css'
 
-
 export default function Button({ loading, active, text, className }) {
   return (
     <button className={`${className} ${loading && 'btn-loading'} ${active && 'btn-active'}`}>
